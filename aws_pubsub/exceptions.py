3a0b5c7198553677f4c29c9469c70e5900b89702
Alex Beach <ajbeach2@gmail.com>
2020-02-16 13:15:00 -0500

    missing file

---
+++ aws_pubsub/exceptions.py
@@ -0,0 +1,16 @@
+class InvalidTaskFunciontDefinition(Exception):
+    """Exception for invalid tasks."""
+
+    pass
+
+
+class InvalidTaskRegistry(Exception):
+    """Task Registration Exception."""
+
+    pass
+
+
+class TaskNotFound(Exception):
+    """Task not found Exception."""
+
+    pass
